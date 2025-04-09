c6c8618fd582f3bc52d2ef030421153e999e0a4f
lucasssantos94 <lucas.ssantos94@outlook.com>
2025-04-09 14:26:14 -0300

    definido expiração do token em 24hrs

--- app/core/config.py
+++ app/core/config.py
@@ -1,4 +1,5 @@
 import os
+from datetime import timedelta
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -6,3 +7,4 @@ load_dotenv()
 class Config:
     DATABASE_URL = os.getenv('DATABASE_URL')
     JWT_SECRET_KEY = 'secret'
+    JWT_ACCESS_TOKEN_EXPIRES = timedelta(hours=24)
